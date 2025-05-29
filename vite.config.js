4ac524bbfa4ca84bfa55e50c270c9aed6e06d962
ishanrana363 <ishanrana363@gmail.com>
2025-05-29 12:30:20 -0800

    vercel file added

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/-
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist",
+    outDir: 'dist', // Vercel looks for this by default
   },
-  server: {
-    host: "0.0.0.0",
-    port: "5175",
-  },
-});
+})
